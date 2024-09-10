432a74d5f932c3a741997c7c67b753aa9a825683
sshatil <mutasimshatil@hotmail.com>
2024-09-11 07:02:23 +0800

    auth middleware & modify supabase client

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,3 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  );
-}
+export const supabase = createClientComponentClient();
