fbe836461d49b1996e0da8cfd4bdb64e842f1595
sshatil <mutasimshatil@hotmail.com>
2024-09-27 20:47:36 +0800

    google auth bug fixed

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,28 +5,26 @@ import { createClient } from "@/lib/supabase/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
+  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host");
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
-
-      // If local, redirect to localhost, else use production domain
-      const finalOrigin = isLocalEnv
-        ? "http://localhost:3000"
-        : process.env.NEXT_PUBLIC_PRODUCTION_URL;
-
-      if (forwardedHost && !isLocalEnv) {
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
-        return NextResponse.redirect(`${finalOrigin}${next}`);
+        return NextResponse.redirect(`${origin}${next}`);
       }
     }
   }
 
+  // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
