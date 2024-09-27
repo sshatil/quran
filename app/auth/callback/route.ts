248864e7acb77e6d277dd77f22edbce4d37f1860
sshatil <mutasimshatil@hotmail.com>
2024-09-27 18:07:26 +0800

    modify auth callback route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,17 +5,28 @@ import { createClient } from "@/lib/supabase/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
   if (code) {
     const supabase = createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
+
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      const forwardedHost = request.headers.get("x-forwarded-host");
+      const isLocalEnv = process.env.NODE_ENV === "development";
+
+      // If local, redirect to localhost, else use production domain
+      const finalOrigin = isLocalEnv
+        ? "http://localhost:3000"
+        : process.env.NEXT_PUBLIC_PRODUCTION_URL;
+
+      if (forwardedHost && !isLocalEnv) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${finalOrigin}${next}`);
+      }
     }
   }
 
-  // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
